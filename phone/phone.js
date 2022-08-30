912126f571b166625e3017884277b9b759f0e867
mosraful21 <mosraful21@gmail.com>
2022-08-30 19:45:55 +0600

    Phone Shop

---
+++ phone/phone.js
@@ -0,0 +1,33 @@
+const loadPhones = async(searchText) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+
+const displayPhones = (phones) => {
+    const phonesContainer = document.getElementById('phones-container');
+    phonesContainer.innerHTML = '';
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card p-4 border border-0">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        </div>
+        `
+        phonesContainer.appendChild(phoneDiv);
+    })
+}
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchFelid = document.getElementById('search-felid');
+    const searchText = searchFelid.value;
+    loadPhones(searchText);
+})
+
+loadPhones();
